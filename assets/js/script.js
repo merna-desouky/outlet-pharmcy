66d67a80b729ef155ab69eff7d2d9cc576be4de6
Merna21 <mernadesoky@gmail.com>
2023-10-19 17:32:46 +0300

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,24 @@ var swiper = new Swiper(".blog-swiper", {
     },
   },
 });
+var swiper = new Swiper(".offer-swiper", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+});
